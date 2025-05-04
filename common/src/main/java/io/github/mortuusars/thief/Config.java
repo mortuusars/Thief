abdaea36238ecb6ed0ccf0424a645159793c68ce
mortuusars <mortuusars@gmail.com>
2025-05-04 22:31:24 +0300

    Fix shift+click trade reputation

--- common/src/main/java/io/github/mortuusars/thief/Config.java
+++ common/src/main/java/io/github/mortuusars/thief/Config.java
@@ -9,8 +9,19 @@ public class Config {
     public static class Server {
         public static final ModConfigSpec SPEC;
 
+        // Misc
+        public static final ModConfigSpec.BooleanValue FIX_SHIFT_CLICK_TRADE_REPUTATION;
+
         static {
             ModConfigSpec.Builder builder = new ModConfigSpec.Builder();
+
+            {
+                builder.push("Misc");
+                FIX_SHIFT_CLICK_TRADE_REPUTATION = builder
+                        .comment("Fix villager trade reputation (gossip) not applying properly when using shift+click to trade (shift+clicking is considered one trade operation, instead of many). Default: true.")
+                        .define("fix_shift_click_villager_trading_reputation", true);
+                builder.pop();
+            }
 
             SPEC = builder.build();
         }
---
+++ common/src/main/java/io/github/mortuusars/thief/mixin/fix_trade_reputation/VillagerMixin.java
@@ -0,0 +1,36 @@
+package io.github.mortuusars.thief.mixin.fix_trade_reputation;
+
+import io.github.mortuusars.thief.Config;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.village.ReputationEventType;
+import net.minecraft.world.entity.npc.AbstractVillager;
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Villager.class)
+public abstract class VillagerMixin extends AbstractVillager {
+    @Shadow private @Nullable Player lastTradedPlayer;
+
+    public VillagerMixin(EntityType<? extends AbstractVillager> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Inject(method = "rewardTradeXp", at = @At("HEAD"))
+    private void onRewardTradeXp(MerchantOffer offer, CallbackInfo ci) {
+        if (Config.Server.FIX_SHIFT_CLICK_TRADE_REPUTATION.get()
+                && lastTradedPlayer != null && level() instanceof ServerLevel serverLevel) {
+            serverLevel.onReputationEvent(ReputationEventType.TRADE, this.lastTradedPlayer, (Villager)(Object)this);
+            level().broadcastEntityEvent(this, (byte)14);
+            lastTradedPlayer = null;
+        }
+    }
+}
