afb4b721d7c3990dbfa4d5e09b967963c54a405f
mortuusars <mortuusars@gmail.com>
2025-05-24 07:56:01 +0300

    guard angry particles
    (cherry picked from commit b97632aa98f0acc6626d48488937244e07a1324b)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## UNRELEASED
+- Guard witnesses (Iron Golem, etc) will emit angry particles when crime is witnessed.
+
 ## 1.0.0 - 1.20.1 - 2025-05-17
 - Backport to 1.20.1
 
--- common/src/main/java/io/github/mortuusars/thief/api/witness/WitnessReaction.java
+++ common/src/main/java/io/github/mortuusars/thief/api/witness/WitnessReaction.java
@@ -2,6 +2,7 @@ package io.github.mortuusars.thief.api.witness;
 
 import io.github.mortuusars.thief.Thief;
 import io.github.mortuusars.thief.world.Crime;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EntityEvent;
 import net.minecraft.world.entity.LivingEntity;
@@ -37,6 +38,18 @@ public class WitnessReaction {
                     && witness.canAttack(criminal)
                     && neutralMob.getTarget() == null) { // Only if not already attacking something
                 neutralMob.setTarget(criminal); // Attack
+
+                for (int i = 0; i < 5; i++) {
+                    double d = witness.getRandom().nextGaussian() * 0.02;
+                    double e = witness.getRandom().nextGaussian() * 0.02;
+                    double f = witness.getRandom().nextGaussian() * 0.02;
+                    level.sendParticles(ParticleTypes.ANGRY_VILLAGER,
+                            witness.getRandomX(1.0),
+                            witness.getRandomY() + 1.0,
+                            witness.getRandomZ(1.0),
+                            5, d, e, f, 0.01);
+                }
+
                 return true;
             }
             return false;
